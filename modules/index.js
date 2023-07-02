2fc2bd8a870506e689759d7cf1ac525d8aab8534
Arteks718 <artexbro.official@gmail.com>
2023-07-02 09:06:43 +0300

    Read file in dir

---
+++ modules/index.js
@@ -0,0 +1,29 @@
+import fs from "fs";
+import util from "util";
+const readAsync = util.promisify(fs.readFileSync);
+const contentDir = fs.readdirSync(".");
+console.log(contentDir);
+const arr = [];
+// contentDir
+//   .filter((file) => file.endsWith(".js"))
+//   .forEach((file) => {
+//     console.log(file)
+//     console.log(fs.readFileSync(file, {encoding: "utf-8"})) 
+//     console.log('---------------------------------')
+//     readAsync(file, { encoding: "utf8" })
+//     .then((data: any) => console.log(data))
+//     .catch((err: any) => console.log(err))
+//   }
+//   );
+contentDir
+    .filter(file => file.endsWith('.js'))
+    .forEach(file => fs.readFile(file, { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+}));
+// readAsync(file, { encoding: "utf8" })
+// .then((data: any) => arr.push(data))
+// .catch((err: any) => console.log(err))
