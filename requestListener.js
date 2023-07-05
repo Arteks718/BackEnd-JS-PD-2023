10d4b5dc525feb31d6a88be5392c79d0e422cd7e
Arteks718 <artexbro.official@gmail.com>
2023-07-05 11:33:13 +0300

    Request server: method write

--- requestListener.js
+++ requestListener.js
@@ -1,38 +1,10 @@
-import path from "path";
-import fs from "fs";
-import util from "util";
-const readFile = util.promisify(fs.readFile);
-let k = 0;
+import url from "url";
 const requestListener = (req, res) => {
-    const { url, method } = req;
-    console.log("client request: ", url, method);
-    if (method == "GET") {
-        const page = url == "/" ? "about.html" : url;
-        const regHTMLPage = /^.*\.html$/;
-        if (regHTMLPage.test(page)) {
-            const __dirname = path.dirname("./pages/");
-            const pagePath = path.join(__dirname, "/pages/", page);
-            if (fs.existsSync(pagePath)) {
-                readFile(pagePath)
-                    .then((data) => {
-                    res.statusCode = 200;
-                    res.setHeader("Content-Type", "text/html");
-                    res.end(data);
-                })
-                    .catch((error) => {
-                    res.statusCode = 500;
-                    console.log("asdasd");
-                    res.end("Server data error", error);
-                });
-            }
-            else {
-                readFile(path.join(__dirname, "/pages/", "404.html")).then((data) => {
-                    res.statusCode = 404;
-                    res.setHeader("Content-Type", "text/html");
-                    res.end(data);
-                });
-            }
-        }
-    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello Client\n');
+    const data = url.parse(req.url, true).query;
+    console.dir(data);
+    res.write(`url: ${req.url}`);
+    res.end();
 };
 export { requestListener };
--- requestListener.ts
+++ requestListener.ts
@@ -2,43 +2,15 @@ import http from "http";
 import path from "path";
 import fs from "fs";
 import util from "util";
+import url from "url";
 
-const readFile = util.promisify(fs.readFile);
-
-let k: number = 0;
-const requestListener = (
-  req: http.IncomingMessage,
-  res: http.ServerResponse
-) => {
-  const { url, method } = req;
-  console.log("client request: ", url, method);
-  if (method == "GET") {
-    const page: any = url == "/" ? "about.html" : url;
-    const regHTMLPage = /^.*\.html$/;
-    if (regHTMLPage.test(page)) {
-      const __dirname = path.dirname("./pages/");
-      const pagePath = path.join(__dirname, "/pages/", page);
-      if (fs.existsSync(pagePath)) {
-        readFile(pagePath)
-          .then((data) => {
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "text/html");
-            res.end(data);
-          })
-          .catch((error) => {
-            res.statusCode = 500;
-            console.log("asdasd");
-            res.end("Server data error", error);
-          });
-      } else {
-        readFile(path.join(__dirname, "/pages/", "404.html")).then((data) => {
-          res.statusCode = 404;
-          res.setHeader("Content-Type", "text/html");
-          res.end(data);
-        });
-      }
-    }
-  }
+const requestListener = (req: any, res: any) => {
+  res.writeHead(200, { 'Content-Type': 'text/html'});
+  res.write('Hello Client\n');
+  const data = url.parse(req.url, true).query;
+  console.dir(data)
+  res.write(`url: ${req.url}`);
+  res.end();
 };
 
 export { requestListener };
