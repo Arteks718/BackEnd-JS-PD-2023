9e518fd4261d57b87061fb3fa1fc7df4be5ddd8b
Arteks718 <artexbro.official@gmail.com>
2023-07-06 12:07:12 +0300

    CRUD users

--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
 import express from 'express'
 import path from 'path'
 const app = express();
+
 app.use(express.json())
 app.use('/', express.static("public"))
 
-app.use('*', (req, res) => {
-  const __dirname = 'public'
-  res.status(404).sendFile(path.resolve(__dirname, '404.html'));
-})
+//CRUD for user
+
+// get all users
+app.get('/users', (req, res) => {})
+// get user by id
+app.get('/users/id', (req, res) => {})
+// create new user
+app.post('/users', (req, res) => {})
+// update user by id
+app.patch('/users/id', (req, res) => {})
+// delete user by id
+app.delete('/users/id', (req, res) => {})
 
 export default app;
-
