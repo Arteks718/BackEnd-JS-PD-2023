8b8d01758bc317ed0dae5882b10928da6d318db5
Arteks718 <artexbro.official@gmail.com>
2023-07-20 17:02:09 +0300

    Add migration and model

---
+++ db/models/user.js
@@ -0,0 +1,64 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { 
+        isAlpha: true, 
+        len: [2, 64]
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { 
+        isAlpha: true, 
+        len: [2, 64]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    passwordHash: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    birthday: {
+      type: DataTypes.DATEONLY,
+      validate: {
+        isDate: true,
+        isBefore: new Date().toLocaleDateString()
+      }
+    },
+    gender: {
+      type: DataTypes.STRING,
+      validate: {
+        isIn: ['male', 'female', 'other']
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'user',
+    underscored: true
+  });
+  return User;
+};
