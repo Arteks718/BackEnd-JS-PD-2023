2744628438ac5e44c849d8e04a1d15c4028edf54
Arteks718 <artexbro.official@gmail.com>
2023-08-09 17:55:12 +0300

    Add fileFilter

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,3 +1,24 @@
-const multer = require('multer');
-const upload = multer({dest: 'public/images'})
-module.exports.uploadHeroFile = upload.single('heroPhoto')
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  // save path
+  destination: (req, file, cb) => {
+    cb(null, "public/images/");
+  },
+  // new file name
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const MIMETYPES = ["image/jpeg", "image/png", "image/gif", "image/webp"];
+
+  cb(
+    null,
+    MIMETYPES.some((mimetype) => mimetype === file.mimetype)
+  );
+};
+
+const upload = multer({ storage, fileFilter });
+module.exports.uploadHeroFile = upload.single("heroPhoto");
