087fdd90e9a0731136fa0c646bc503a3b23d64a7
Arteks718 <artexbro.official@gmail.com>
2023-08-22 14:44:44 +0300

    Refactor controller errors

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,86 +1,87 @@
-const _ = require('lodash');
-const { Task } = require('../db/models')
+const _ = require("lodash");
+const createHttpError = require("http-errors");
+const { Task } = require("../db/models");
 
 module.exports = {
-  getAllTask:async (req, res) => {
+  getAllTask: async (req, res, next) => {
     try {
       const foundTasks = await Task.findAll({
         raw: true,
-        attributes: { exclude: ['updatedAt']}
-      })
+        attributes: { exclude: ["updatedAt"] },
+      });
 
-      res.status(200).send(foundTasks)
+      res.status(200).send(foundTasks);
     } catch (error) {
-      res.status(500).send('Server error')
+      next(error);
     }
   },
-  getTaskById:async (req, res) => {
-    const { taskId } = req.params
+  getTaskById: async (req, res, next) => {
+    const { taskId } = req.params;
     try {
-      const foundTask = await Task.findByPk(taskId,{
+      const foundTask = await Task.findByPk(taskId, {
         raw: true,
-        attributes: { exclude: ['updatedAt']}
-      })
-      if(!foundTask){
-        return res.status(404).send(`Task not found`)
+        attributes: { exclude: ["updatedAt"] },
+      });
+      if (!foundTask) {
+        return next(createHttpError(404, `Task not found`));
       }
-      res.status(200).send(foundTask)
+      res.status(200).send(foundTask);
     } catch (error) {
-      res.status(500).send('Server error')
+      next(error);
     }
   },
-  createTask:async (req, res) => {
+  createTask: async (req, res, next) => {
     const { body } = req;
     try {
-      const createdTask = await Task.create(body)
-      if(!createdTask) {
-        console.log(createdTask)
-        return res.status(500).send(`Server error`)
+      const createdTask = await Task.create(body);
+      if (!createdTask) {
+        console.log(createdTask);
+        return next(createHttpError(500, `Server error`));
       }
-      const preparedTask = _.omit(createdTask.get(), ['updatedAt']);
-      res.status(201).send(preparedTask)
+      const preparedTask = _.omit(createdTask.get(), ["updatedAt"]);
+      res.status(201).send(preparedTask);
     } catch (error) {
-      console.log(error)
-      res.status(500).send(`Server error`)
+      next(error);
     }
   },
-  updateTaskById:async (req, res) => {
-    const {body, params: {taskId}} = req;
+  updateTaskById: async (req, res, next) => {
+    const {
+      body,
+      params: { taskId },
+    } = req;
     try {
       const [, [updatedTask]] = await Task.update(body, {
         where: {
-          id: taskId
+          id: taskId,
         },
         raw: true,
-        attributes: { exclude : ['updatedAt']},
-        returning: true
-      })
+        attributes: { exclude: ["updatedAt"] },
+        returning: true,
+      });
 
-      if(!updatedTask) {
-        return res.status(404).send('Task not updated')
+      if (!updatedTask) {
+        return next(createHttpError(404, "Task not found"));
       }
-      res.status(200).send(updatedTask)
+      res.status(200).send(updatedTask);
     } catch (error) {
-      console.log(error)
-      res.status(500).send(`Server error`)
+      next(error);
     }
   },
-  deleteTaskById:async (req, res) => {
+  deleteTaskById: async (req, res, next) => {
     const { taskId } = req.params;
     try {
       const deletedTaskCount = await Task.destroy({
         where: {
-          id: taskId
-        }
-      })
-      if(!deletedTaskCount) {
-        return res.status(404).send('Task not deleted');
+          id: taskId,
+        },
+      });
+      if (!deletedTaskCount) {
+        return next(createHttpError(404, "Task not deleted"));
       }
 
-      res.status(204).end()
+      res.status(204).end();
     } catch (error) {
-      console.log(error)
-      res.status(500).send(`Server error`)
+      next(error);
     }
   },
-}
+};
